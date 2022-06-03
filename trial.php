be191a181b8e36d0a977df07e092f864fd16cd99
Ayimsolomon <90561026+Ayimsolomon@users.noreply.github.com>
2022-06-03 06:20:41 +0100

    Add files via upload

---
+++ trial.php
@@ -0,0 +1,28 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #para{
+            background:green;
+            position:relative;
+            display:flex;
+            justify-content:center;
+        }
+    </style>
+</head>
+<body>
+    <form action="conn.php" method="post">
+        <input type="email" name="Email" id="">
+        <input type="password" name="Password" id="">
+        <input type="text" name="Gender" id="">
+        <input type="submit" name=submit value="submit">
+    </form>
+</body>
+</html>
