9b5f36ed3ea1615fe6a280a99b9e7248299582f0
Ayimsolomon <90561026+Ayimsolomon@users.noreply.github.com>
2022-06-03 06:18:53 +0100

    Add files via upload

---
+++ functional.js
@@ -0,0 +1,8 @@
+    $(document).ready(function(){
+
+  $(".solution").click(function(){
+
+    $('.panel1,.panel2,.panel3,.panel4,.panel5,.panel6,.panel7,.panel8,.panel9').slideToggle("slow");
+});
+
+});
